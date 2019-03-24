af1cf17f61a1df6d2d38b44c88058c59c721b372
Adrian <mazxaxz@gmail.com>
2019-03-24 15:32:32 +0100

    basic configuration, added icons, basic styling

--- projects/ng-ticket-builder/src/lib/ng-ticket-builder.component.ts
+++ projects/ng-ticket-builder/src/lib/ng-ticket-builder.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'mzx-ng-ticket-builder',
-  template: `
-    <p>
-      ng-ticket-builder works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './ng-ticket-builder.component.html',
+  styleUrls: ['./ng-ticket-builder.component.scss']
 })
 export class NgTicketBuilderComponent implements OnInit {
+  @ViewChild('builderCanvas') builderCanvas: any;
+  private _canvas: any;
 
-  constructor() { }
+  constructor(private renderer2: Renderer2) { }
 
   ngOnInit() {
+    this._canvas = this.builderCanvas.nativeElement;
+    this._initialize();
+  }
+
+  private _initialize() {
+    this.renderer2.setStyle(this._canvas, 'background-color', '#fff');
+    this.renderer2.setStyle(this._canvas, 'width', '50%');
+    this.renderer2.setStyle(this._canvas, 'height', '50%');
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgTicketBuilderModule } from 'projects/ng-ticket-builder/src/public_api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgTicketBuilderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
