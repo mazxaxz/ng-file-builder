054a4ad899f00b0827b23b9c8be0b7271e777d4e
Adrian <mazxaxz@gmail.com>
2019-03-24 16:22:37 +0100

    styling, and implemented change orientation

--- projects/ng-ticket-builder/src/lib/ng-ticket-builder.component.ts
+++ projects/ng-ticket-builder/src/lib/ng-ticket-builder.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit, ViewChild, Renderer2 } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2, ViewChildren, QueryList } from '@angular/core';
+
+enum Size {
+  A5 = "A5",
+  A4 = "A4",
+  A3 = "A3"
+}
+
+enum Orientation {
+  Horizontal = "horizontal",
+  Vertical = "vertical"
+}
 
 @Component({
   selector: 'mzx-ng-ticket-builder',
@@ -7,7 +18,13 @@ import { Component, OnInit, ViewChild, Renderer2 } from '@angular/core';
 })
 export class NgTicketBuilderComponent implements OnInit {
   @ViewChild('builderCanvas') builderCanvas: any;
+  @ViewChildren('sizeAction') sizeActions: QueryList<any>;
   private _canvas: any;
+  currentSize: Size;
+  currentOrientation: Orientation;
+
+  Size = Size;
+  Orientation = Orientation;
 
   constructor(private renderer2: Renderer2) { }
 
@@ -16,10 +33,37 @@ export class NgTicketBuilderComponent implements OnInit {
     this._initialize();
   }
 
+  changeOrientation() {
+    const width = this._canvas.style.width;
+    const height = this._canvas.style.height;
+    this.renderer2.setStyle(this._canvas, 'width', height);
+    this.renderer2.setStyle(this._canvas, 'height', width);
+
+    if (this.currentOrientation === Orientation.Horizontal) {
+      return this.currentOrientation = Orientation.Vertical;
+    }
+
+    this.currentOrientation = Orientation.Horizontal;
+  }
+
+  setSize(size: Size) {
+    this.currentSize = size;
+    this.sizeActions.forEach(action => {
+      const nativeElement = action.nativeElement;
+      if (nativeElement.dataset.size === size) {
+        return this.renderer2.addClass(nativeElement, 'active');
+      }
+
+      return this.renderer2.removeClass(nativeElement, 'active');
+    });
+  }
+
   private _initialize() {
+    this.currentSize = Size.A4;
+    this.currentOrientation = Orientation.Vertical;
     this.renderer2.setStyle(this._canvas, 'background-color', '#fff');
-    this.renderer2.setStyle(this._canvas, 'width', '50%');
-    this.renderer2.setStyle(this._canvas, 'height', '50%');
+    this.renderer2.setStyle(this._canvas, 'width', '200px');
+    this.renderer2.setStyle(this._canvas, 'height', '400px');
   }
 
 }
